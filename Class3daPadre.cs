8cb32e648c291160017fde5586909452d8567265
zioIgna <ignaziocarbonaro@hotmail.com>
2025-06-27 19:35:16 +0200

    Aggiunta da Figlio Classe creata da padre

---
+++ Class3daPadre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padre.GitTestFiglio1Lib
+{
+    internal class Class3daPadre
+    {
+    }
+}
